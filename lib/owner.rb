f4858d56d232670f22574d520972638e6fc13f63
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 01:35:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets, :dog, :cat
+  
+  @@all = []
+  @@pets ={:dog => [], :cats => []}
+  
+  def initialize(species)
+    @species = species
+    @name = name
+    @all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
 end
